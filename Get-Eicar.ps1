41b74dd1207a0d05a842d738d205973b263b6261
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2021-02-20 10:03:55 -0600

    Update Get-Eicar.ps1

--- Get-Eicar.ps1
+++ Get-Eicar.ps1
@@ -1,4 +1,25 @@
-Function Get-Eicar {
+function Get-Eicar {
+
+<#
+
+.Synopsis
+
+            Get-Eicar
+            Author: Neil Hartsfield
+            
+.Description
+
+            The EICAR test file is simply a 68 character long string used against AV products for testing purposes. 
+            This is a function which drops an EICAR test file (.txt) on the local user's Desktop to test a system's On-Access Scanner. 
+            A detection should be made immediately if the system's OAS is working properly. 
+
+.Example
+
+            PS C:> Get-Eicar
+            Eicar dropped at ~\Desktop\EicarTest.txt.
+
+#>
+
 
 #Base64 of Eicar string
 $Base64Eicar = 'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
