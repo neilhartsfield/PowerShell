960659107dee372f3f1110a3b581cc537316e27e
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2021-02-20 09:53:51 -0600

    Create Get-Eicar.ps1

---
+++ Get-Eicar.ps1
@@ -0,0 +1,28 @@
+Function Get-Eicar {
+
+#Base64 of Eicar string
+$Base64Eicar = 'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
+
+$Path = '~/Desktop'
+$FilePath = Join-Path $Path EicarTest.txt
+
+
+    If (!(Test-Path -Path $FilePath)) {
+
+            Try {
+
+            $EicarString = [System.Convert]::FromBase64String($Base64Eicar)
+            $Eicar = [System.Text.Encoding]::UTF8.GetString($EicarString)
+            Set-Content -Value $Eicar -Encoding ASCII -Path $FilePath -Force
+            Write-Output "Eicar dropped at $FilePath."
+            }
+
+            Catch {
+            Write-Warning "Eicar file could not be created."
+            }
+}
+     Else {
+        Write-Warning "Eicar already exists at $FilePath."
+}
+
+}
