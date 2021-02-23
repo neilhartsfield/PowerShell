155723e9a69abadd241a3f4d7527d61e6aae9d95
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2021-02-23 16:21:15 -0600

    Update Get-Eicar.ps1

--- Get-Eicar.ps1
+++ Get-Eicar.ps1
@@ -4,35 +4,32 @@ function Get-Eicar {
 
 .Synopsis
 
-            Get-Eicar
-            Author: Neil Hartsfield
+	Get-Eicar
+	Author: Neil Hartsfield
             
 .Description
 
-            The EICAR test file is simply a 68 character long string used against AV products for testing purposes. 
-            This is a function which drops an EICAR test file (.txt) on the local user's Desktop to test a system's On-Access Scanner. 
-            A detection should be made immediately if the system's OAS is working properly. 
+	The EICAR test file is simply a 68 character long string used against AV products for testing purposes. 
+	This is a function which drops an EICAR test file (.txt) on the local user's Desktop to test a system's On-Access Scanner. 
+	A detection should be made immediately if the system's OAS is working properly. 
             
-            We can obtain the correct path for OAS exclusions by dropping EICAR files in the directories we want to exclude. 
+	We can obtain the correct path for OAS exclusions by dropping EICAR files in the directories we want to exclude. 
 	Once the EICAR detection is made, we can look at the OAS logs and see exactly how the scan engine is seeing these directories. 
 	By knowing how our engine scans the directory, we know how to configure the OAS exclusion.
             
-            When executed, this PS script will prompt you with a target directory you would like to drop the EICAR test file in.
-            Once the EICAR is dropped, PowerShell will wait and watch for a detection being made by your OAS.
-            Upon detection, a log snippet will display, showing exactly how the AV scan engine is seeing the directory.
+	When executed, this PS script will prompt you with a target directory you would like to drop the EICAR test file in.
+	Once the EICAR is dropped, PowerShell will wait and watch for a detection being made by your OAS.
+	Upon detection, a log snippet will display, showing exactly how the AV scan engine is seeing the directory.
 
 .Example
 
-            PS C:> Get-Eicar
-            Target directory to test EICAR detection (full path): C:\My
+	PS C:> Get-Eicar
+	Target directory to test EICAR detection (full path): C:\My
 	Eicar dropped at C:\My\EicarTest.txt.
             
-            2021-02-23 21:54:21.543Z | WIN2019\Administrator ran
-	C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe, which attempted to access C:\My\EicarTest.txt. The Test
-	named EICAR test file was detected and deleted.
+	<Log Snippet Here Showing Detection>
 
 #>
-
 
 #Base64 of Eicar string
 $Base64Eicar = 'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
@@ -57,6 +54,7 @@ $FilePath = Join-Path $Path EicarTest.txt
     Else {
     Write-Warning "Eicar already exists at $FilePath."
     }
+    
+    Get-Content "%LOGDIRECTORYHERE%" -tail 2 -wait | select-string 'EicarTest'
+    
 }
-
-Get-Content "%LOGDIRECTORY%" -tail 2 -wait | select-string 'EICAR'
