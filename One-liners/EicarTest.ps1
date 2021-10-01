16be8b097eb51bde229c09be7fa9eb8d5fd41359
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2021-10-01 14:34:30 -0500

    Update EicarTest.ps1

--- One-liners/EicarTest.ps1
+++ One-liners/EicarTest.ps1
@@ -1,6 +1,6 @@
 <#
 Generates an EICAR test file in current working directory. We need to escape (`) certain special characters (\, $).
-Batch version found here: https://github.com/neilhartsfield/Batch/blob/main/EicarTest.bat
+Batch script version found here: https://github.com/neilhartsfield/Batch/blob/main/EicarTest.bat
 
 Actual : X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*
 Escape : X5O!P%@AP[4`\PZX54(P^)7CC)7}`$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!`$H+H*
