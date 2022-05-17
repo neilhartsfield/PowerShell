22fe88effdc7114e7dda71aca8b10cdfc826b311
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2022-05-17 11:11:46 -0500

    Create UninstallWithPW.ps1

---
+++ UninstallWithPW.ps1
@@ -0,0 +1,21 @@
+# Log actions in the %temp% directory
+$Timestamp = Get-Date -Format "yyyy-MM-dd_THHmmss"
+$LogFile = "$env:TEMP\WCUninst_$Timestamp.log"
+
+#Base64 of uninstall password
+$Base64PW = 'cGFzc3dvcmQ='
+
+$PWString = [System.Convert]::FromBase64String($Base64PW)
+$PW = [System.Text.Encoding]::UTF8.GetString($PWString)
+
+# Set App variable to appropriate ENS module
+$App = "C:\Program Files (x86)\McAfee\Endpoint Security\Web Control\RepairCache\setupWC.exe"
+
+# Loop through and uninstall any matches
+foreach ($a in $App) {
+	$Params = @(
+		"/x"
+		"/PASSWORD=""$PW"""
+	)
+	Start-Process $App -ArgumentList $Params -Wait -NoNewWindow
+}
